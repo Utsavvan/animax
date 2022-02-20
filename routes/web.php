27729cc5d5bb3a719b254ab86e82046483e69601
Utsav <utsav1pariya@gmail.com>
2022-02-20 09:19:18 +0530

    website pages added

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 
 // Authentication routes
@@ -22,3 +12,24 @@ Route::group(['prefix' => 'admin'], function () {
 
 // Include Wave Routes
 Wave::routes();
+
+//website routes
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/subscription', function () {
+    return view('subscription');
+});
+
+Route::get('/support', function () {
+    return view('support');
+});
